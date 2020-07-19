265d5b928f6ae8f871e6337d39e4a257719f47a2
hari <hariprasad.arumughan@gmail.com>
2020-07-19 21:52:27 +0530

    Change FileContains Source Code Supported for V9

--- src/com/jscape/inet/mft/functions/repo/FileContains.java
+++ src/com/jscape/inet/mft/functions/repo/FileContains.java
@@ -1,21 +1,16 @@
 package com.jscape.inet.mft.functions.repo;
 
-import com.jscape.inet.mft.Function;
-import com.jscape.util.io.primitive.CommonLineReader;
 
-import java.io.InputStream;
+import com.jscape.inet.mft.Function;
+
+import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
-/**
- * Created by hari on 8/28/2018.
- */
 public class FileContains extends Function {
 
     private static final int MAX_LINE_LENGTH = 65536;
@@ -35,32 +30,33 @@ public class FileContains extends Function {
     @Override
     public Map<String, String> getTemplates() {
         HashMap<String, String> map = new HashMap<String, String>();
-        map.put("FileContains(filepath,regex)", "Returns true if regular expression matches any content in the file on default character set and default max lines");
-        map.put("FileContains(filepath,regex,maxlines)", "Returns true if regular expression matches any content in the file on specified maximum lines");
-        map.put("FileContains(filepath,regex,maxlines,charset)", "Returns true if regular expression matches any content in the file on specified maximum lines and charset");
+        map.put("FileContainsCopy(filepath,regex)", "Returns true if regular expression matches any content in the file");
+        map.put("FileContainsCopy(filepath,regex,maxlines)", "Returns true if regular expression matches any content in the file on specified maximum lines");
         return map;
     }
 
     @Override
     public Object evaluate(Object[] value) {
-        try {
-            Path file = Paths.get(value[0].toString());
-            Pattern pattern = Pattern.compile(value[1].toString(),Pattern.CASE_INSENSITIVE);
-            int maxLineLength = value.length > 2 ? Integer.parseInt(value[2].toString()) : MAX_LINE_LENGTH;
-            Charset charset = value.length > 3 ? Charset.forName(value[3].toString()) : CHARSET;
 
-            try (InputStream in = Files.newInputStream(file)) {
-                CommonLineReader reader = new CommonLineReader(in, maxLineLength);
-                String line;
-                int lineNo =0;
-                while ((line = reader.readLine(charset)) != null) {
-                    lineNo++;
-                    if (lineNo <=maxLineLength && pattern.matcher(line).find()) {
-                        return true;
-                    }
+        File file = new File(value[0].toString());
+        String phrase = value[1].toString();
+        int maxLineLength = value.length > 2 ? Integer.parseInt(value[2].toString()) : MAX_LINE_LENGTH;
+        int lineNo = 1;
+        try {
+            Scanner fileScanner = new Scanner(file);
+            Pattern pattern = Pattern.compile(phrase,Pattern.CASE_INSENSITIVE);
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (pattern.matcher(line).matches()){
+                    fileScanner.close();
+                    return true;
                 }
-                return false;
+                lineNo++;
+                if (lineNo > maxLineLength) break;
             }
+            fileScanner.close();
+            return false;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
